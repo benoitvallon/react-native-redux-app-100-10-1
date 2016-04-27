d2620d624968433092c33012235a345cf5371594
Benoit VALLON <benoitvallon@gmail.com>
2016-04-27 00:50:43 -0400

    Add tests for onPress methods on add idea screen

--- src/__tests__/spec/components/AddIdea.js
+++ src/__tests__/spec/components/AddIdea.js
@@ -4,6 +4,7 @@ jest.disableAutomock();
 
 import React, { View } from 'react-native';
 import TestUtils from 'react-addons-test-utils';
+import expect from 'expect';
 
 import { AddIdea } from '../../../components/AddIdea';
 
@@ -56,5 +57,59 @@ describe('add idea page', () => {
     expect(output.props.children[2]).toEqual(undefined);
     expect(output.props.children[3].props.children.props.children).toEqual('Save');
     expect(output.props.children[4].props.children.props.children).toEqual('Remove');
+  });
+
+  it('should be able to press buttons for new idea', () => {
+    const props = {
+      newIdea: true,
+      state: {
+        ideas: [ { title: 'My idea' }]
+      },
+      actions: {
+        add: expect.createSpy(),
+        save: expect.createSpy(),
+        remove: expect.createSpy()
+      },
+      navigator: {
+        pop: expect.createSpy(),
+        push: expect.createSpy()
+      }
+    };
+    const { output } = setup(AddIdea, props);
+
+    const inputText = output.props.children[1];
+    inputText.props.onChangeText('My new idea');
+
+    const addButton = output.props.children[2];
+    addButton.props.onPress();
+    expect(props.actions.add.calls.length).toBe(1);
+  });
+
+  it('should be able to press buttons for existing idea', () => {
+    const props = {
+      index: 0,
+      rowIDDisplay: 1,
+      idea: 'My idea',
+      state: {
+        ideas: [ { title: 'My idea' }]
+      },
+      actions: {
+        add: expect.createSpy(),
+        save: expect.createSpy(),
+        remove: expect.createSpy()
+      },
+      navigator: {
+        pop: expect.createSpy(),
+        push: expect.createSpy()
+      }
+    };
+    const { output } = setup(AddIdea, props);
+
+    const saveButton = output.props.children[3];
+    saveButton.props.onPress();
+    expect(props.actions.save.calls.length).toBe(1);
+
+    const removeButton = output.props.children[4];
+    removeButton.props.onPress();
   });
 });
