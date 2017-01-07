e310e22d42ff4cbda5bf866285340b32d71569ce
Benoit VALLON <benoitvallon@gmail.com>
2017-01-07 11:08:47 -0500

    Add snapshots for actions tests

--- src/__tests__/spec/actions/ideaActions.js
+++ src/__tests__/spec/actions/ideaActions.js
@@ -1,9 +1,6 @@
 'use strict';
 
-jest.disableAutomock();
-
 import * as actions from '../../../actions/ideaActions';
-import * as types from '../../../actions/actionTypes';
 
 describe('idea actions', function() {
   it('should be find an actions file', function() {
@@ -12,39 +9,21 @@ describe('idea actions', function() {
 
   it('should create an action to add an idea', function() {
     const idea = 'My first idea';
-    const expectedAction = {
-      type: types.ADD,
-      data: idea
-    };
-    expect(actions.add(idea)).toEqual(expectedAction);
+    expect(actions.add(idea)).toMatchSnapshot();
   });
 
   it('should create an action to save an idea', function() {
     const index = 1;
     const idea = 'My second idea';
-    const expectedAction = {
-      type: types.SAVE,
-      data: {
-        idea,
-        index
-      }
-    };
-    expect(actions.save(idea, index)).toEqual(expectedAction);
+    expect(actions.save(idea, index)).toMatchSnapshot();
   });
 
   it('should create an action to remove an idea', function() {
     const index = 1;
-    const expectedAction = {
-      type: types.REMOVE,
-      data: index
-    };
-    expect(actions.remove(index)).toEqual(expectedAction);
+    expect(actions.remove(index)).toMatchSnapshot();
   });
 
   it('should create an action to reset the idea list', function() {
-    const expectedAction = {
-      type: types.RESET
-    };
-    expect(actions.reset()).toEqual(expectedAction);
+    expect(actions.reset()).toMatchSnapshot();
   });
 });
